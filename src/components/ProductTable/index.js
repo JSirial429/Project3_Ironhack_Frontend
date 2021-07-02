9466235f46bcab8bc848ee9dbcf22dc10d5eef91
LeonelIG-01 <75810398+LeonelIG-01@users.noreply.github.com>
2021-07-02 03:52:54 -0400

    product table needs data to be tested, sign up form created

---
+++ src/components/ProductTable/index.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { TableContainer, TableWrap } from './TableElements';
+
+
+class ProductTable extends Component {
+
+    constructor(props) {
+        super(props) {
+            this.state = {
+                product: [
+                    { id: 1, name: " ", price: "", url: " " }
+                ]
+            }
+        }
+    }
+    
+    renderTableData() {
+        return this.state.product.map((product, index) => {
+            const { id, name, price, url } = product
+            return (
+                <tr key={id}>
+                    <td>{id}</td>
+                    <td>{name}</td>
+                    <td>{price}</td>
+                    <td>{url}</td>
+                </tr>
+            )
+        })
+    }
+
+    renderTableHeader() {
+        let header = Object.keys(this.state.products[0])
+        return header.map((key, index) => {
+            return <th key={index}>{key.toUpperCase()}</th>
+        })
+    }
+
+    render() {
+    return (
+        <TableContainer>
+            <TableWrap>
+                <table id='products'>
+                    <tr>{this.renderTableHeader}</tr>
+                    {this.renderTableData}
+            </table>
+            </TableWrap>
+        </TableContainer>
+    )
+}
+
+export default ProductTable;
---
+++ src/components/Signup/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Container, FormWrap, Icon, FormContent, Form, FormH1, FormLabel, FormInput, FormButton } from './SignupElement'
+
+const Signup = () => {
+    return (
+        <>
+            <Container>
+                <FormWrap>
+                    <Icon to='/'>Price Checker</Icon>
+                    <FormContent>
+                        <Form onSubmit={signUp}>
+                            <FormH1>Create an account</FormH1>
+                            <FormLabel htmlFor='for'>Email</FormLabel>
+                            <FormInput type='email' required onChange={(e) => setEmail(e.target.value)} value={email} />
+                            <FormLabel htmlFor='for'>Password</FormLabel>
+                            <FormInput type='password' required onChange={(e)=> setPassword(e.target.value)} value={password}/>
+                            <FormButton type='Submit'>Sign Up</FormButton>
+                        </Form>
+                    </FormContent>
+                </FormWrap>
+            </Container>
+        </>    
+    )
+}
+
+export default Signup
