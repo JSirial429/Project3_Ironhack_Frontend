683bdacde84d313c07d9e650aa824028e863c02f
JSirial429 <Jos0928@gmail.com>
2021-07-01 21:13:13 -0400

    Merged changes

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchBar from '../SearchBar';
+<<<<<<< Updated upstream
 import { DashboardContainer, DashH1, DashP, Icon, TableWrap } from './DashboardElements';
+=======
+import { DashboardContainer, DashH1, DashP } from './DashboardElements';
+import service from '../../services/axios.services';
+>>>>>>> Stashed changes
 
 const Dashboard = () => {
+
+    const[product, setProduct] = useState("");
+    const[productResult, setProductResult] = useState("");
+    const[productArr, setProductArr] = useState([]);
+
+    const productSearch = async (e)=>{
+
+        e.preventDefault();
+        service.productLookUp({product})
+        .then( response =>{
+            console.log(response);
+        })
+        .catch( err =>{
+            console.log(err);
+        });
+
+    }
+
     return (
         <DashboardContainer>
                 <TableWrap>
